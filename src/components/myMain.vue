a4858dab360529f9fc3c8496ac6e38b4a2b7b53a
CrisL1990 <lucchetta.cris@gmail.com>
2022-02-23 16:37:02 +0100

    Completato richiesta API

--- src/components/myMain.vue
+++ src/components/myMain.vue
@@ -1,7 +1,10 @@
 <template>
     <main class="container">
         <div class="row">
-
+            <myCard v-for="(disco, index) in dischi.response" 
+            :key="index"
+            :disco="disco"
+            />
         </div>
     </main>
   
@@ -10,12 +13,19 @@
 <script>
 
 const axios = require('axios');
+
+import myCard from './partials/myCard.vue'
+
 export default {
     name: 'myMain',
 
+    components:{
+        myCard,
+    },
+
     data(){
         return{
-            myArray: []
+            dischi: []
         }
        
     },
@@ -23,13 +33,10 @@ export default {
     methods:{
 
         callApi: function(){
-
             axios.get('https://flynn.boolean.careers/exercises/api/array/music')
 
-            .then((response) => {
-                
-                this.myArray = response.data;
-                console.log(this.myArray);
+            .then((response) => {  
+                this.dischi = response.data;
             })
             .catch(function (error) {
                 // handle error
---
+++ src/components/partials/myCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="col">
+        <img :src="disco.poster" alt="">
+        <h2>{{disco.title}}</h2>
+        <div>{{disco.author}}</div>
+        <div>{{disco.year}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'myCard',
+
+    props:{
+        'disco': Object
+    },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
